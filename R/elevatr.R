674cb93883fb1f6b110df695d783f7cd4fa60503
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2016-08-09 16:36:07 -0400

    first commit

---
+++ R/elevatr.R
@@ -0,0 +1,7 @@
+#' Access elevation data from the web
+#' 
+#' This package provides tools to access and download elevation data available
+#' from a variety of online sources. 
+#'                  
+#' @name elevatr
+NULL 
---
+++ R/get_elevation.R
@@ -0,0 +1,15 @@
+#' Get elevation data
+#' 
+#' Primary function for accessing elevation data from a variety of online 
+#' sources
+#' 
+#' @param location  a data frame, \code{sp} object, a \code{bbox}, or 
+#'                  \code{extent} object to get elevation data for.
+#' @param source
+#' @param key
+#' @param out
+#' @export
+#' @examples 
+get_elevation <- function(location, source, key, out){
+  
+}
