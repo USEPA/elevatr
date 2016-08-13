624f45330ddcce9179efed170d4af9d0da1efd03
Jeff Hollister <jeff.w.hollister@gmail.com>
2016-08-12 20:24:09 -0400

    data frame working need to think about engineering.

--- R/get_elevation.R
+++ R/get_elevation.R
@@ -3,20 +3,38 @@
 #' Primary function for accessing elevation data from a variety of online 
 #' sources
 #' 
-#' @param location  a data frame, \code{sp} object, a \code{bbox}, or 
-#'                  \code{extent} object to get elevation data for.
+#' @param location  The location for which you wish to return elevation.  You 
+#'                  may either enter a data frame of Longitude and Latitude, a 
+#'                  \code{sp} object, a \code{bbox}, or \code{extent} object.  A 
+#'                  data frame will return a data frame, and the other object 
+#'                  will return a raster.
 #' @param source
-#' @param key
-#' @param out
+#' @param units
+#'
 #' @importFrom httr GET
 #' @importFrom jsonlite::fromJSON
 #' @export
 #' @examples 
-get_elevation <- function(location, source, key, out){
-  url <- "http://ned.usgs.gov/epqs/pqs.php?x=-72&y=42&units=Meters&output=json"
-  resp <- GET(url)
-  if (http_type(resp) != "application/json") {
-    stop("API did not return json", call. = FALSE)
+get_elevation <- function(location, source = c("epqs","srtm"), 
+                          units = c("Meters","Feet")){
+  source <- match.arg(source)
+  units <- match.arg(units)
+  if(source == "epqs"){
+  df <- data.frame(matrix(ncol = 3, nrow = nrow(location)))
+  base_url <- "http://ned.usgs.gov/epqs/pqs.php?"
+  units <- paste0("&units=",units)
+    for(i in seq_along(location[,1])){
+      x <- location[i,1]
+      y <- location[i,2]
+      loc <- paste0("x=",x, "&y=", y)
+      url <- paste0(base_url,loc,units,"&output=json")
+      resp <- GET(url)
+      if (http_type(resp) != "application/json") {
+        stop("API did not return json", call. = FALSE)
+      } 
+      resp <- fromJSON(content(resp, "text"), simplifyVector = FALSE)
+      df[i,] <- c(x,y,resp[[1]][[1]]$Elevation)
+    }
+  df
   }
-  resp
 }
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # elevatr
 An R package for accessing elevation data
+
+This package will provide an interface to the USGS Elevation Point Query Service 
+and for global 
