1086784c03a025326596a07c9477c0cb09e72f6f
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2016-10-26 13:17:15 -0400

    Updated README with more details on package api

--- R/get_elevation.R
+++ R/get_elevation.R
@@ -1,4 +1,4 @@
-#' Get elevation data
+#' Get elevation data from the USGS Elevation Point Query Service (eqps)
 #' 
 #' Primary function for accessing elevation data from a variety of online 
 #' sources
@@ -6,35 +6,11 @@
 #' @param location  A data.frame of the location(s) for which you wish to return 
 #'                  elevation. The first colum is Longitude and the second 
 #'                  column is Latitude.  
-#' @param source  character string indicating 'epqs' for USGS elevation point 
-#'                query service or 'srtm' for Open Topography SRTM service
 #' @param units Character string of either meters or feet.  Only works for 'epqs'
-#' @param res Character string of either 30 or 90. Only works for 'srtm' and 
-#'            fetches data for the 30 meter SRTM (i.e., SRTM GL1 (Global 30m)) or
-#'            the 90 meter SRTM (i.e., SRTM GL3 (Global 90m))
 #' @export
 #' @examples 
 #' xdf<-data.frame(runif(10,-75,-72),runif(10,40,45))
-#' get_elevation(xdf)
-#' get_elevation(xdf,"srtm")
-get_elevation <- function(location, source = c("epqs","srtm"), 
-                          units = c("Meters","Feet"),
-                          res = c("30","90")){
-  source <- match.arg(source)
-  units <- match.arg(units)
-  res <- match.arg(res)
-  if(source == "epqs"){
-    return(get_epqs(location,units))
-  }
-  if(source == "srtm"){
-    return(get_srtm(location,res))
-  }
-}
-
-#' Get EPQS
-#' @keywords internal
-#' @param location location from get_elevation
-#' @param units units from get_elevation
+#' get_eqps(xdf)
 get_epqs <- function(location,units){
   df <- data.frame(matrix(ncol = 3, nrow = nrow(location)))
   base_url <- "http://ned.usgs.gov/epqs/pqs.php?"
@@ -58,42 +34,12 @@ get_epqs <- function(location,units){
   df
 }
 
-#' Get SRTM from Open Topography
-#' @keywords internal
-#' @param location location from get_elevation
-#' @param res res from get_elevation
-get_srtm <- function(location,res){
-   browser()
-   df <- data.frame(matrix(ncol = 3, nrow = nrow(location)))
-   base_url <- "http://opentopo.sdsc.edu/otr/getdem?"
-   if(res == 90){dem <- "demtype=SRTMGL3"}
-   if(res == 30){dem <- "demtype=SRTMGL1"}
-   #NEED FUNCTION TO SET LOCATION FOR a single SRTM download
-   west <- location[i,1]
-   east <- west + 10
-   north <- location[i,2]
-   south <- north - 10
-   loc <- paste0("&west=",west,"&south=",south,"&east=",east,"&north=",north)
-   url <- paste0(base_url,dem,loc)
-   tmp_gtif <- tempfile(fileext=".tif")
-   resp <- httr::POST(url,httr::write_disk(tmp_gtif,overwrite=T))
-   #NEED TO FIGURE OUT RESPONSE TYPE (octet-stream?)
-   if (httr::http_type(resp) != "application/octet-stream") {
-     stop("API did not return json", call. = FALSE)
-   } 
-   for(i in seq_along(location[,1])){
- 
-     #NEED FUNCTION TO GET SINGLE POINT ELEVATION FROM RASTER
-     df[i,] <- c(x,y,resp[[1]][[1]]$Elevation)
-   }
-   df
-}
-
 #' Get a digital elevation model from the Mapzen Terrain Tiles
 #' 
 #' This function uses the Mapzen Terrain Tile service to retrieve an elevation
-#' raster.  It accepts a \code{sp::bbox} object as input and returns a single
-#' raster object covering that extent.  You must have an api_key from Mapzen.
+#' raster from the geotiff service.  It accepts a \code{sp::bbox} object as 
+#' input and returns a single raster object covering that extent.  You must have
+#' an api_key from Mapzen.
 #' 
 #' @param bbx a \code{sp::bbox} object that defines the area to return
 #' @param z The zoom level to return.  The zoom ranges from 1 to 15.  Resolution
@@ -103,9 +49,6 @@ get_srtm <- function(location,res){
 #' @param api_key An API Key from Mapzen, create at 
 #'                \url{https://mapzen.com/developer} Required. Set in your 
 #'                \code{.Rprofile} file with the option \code{mapzen_key}
-#' @param file_format Several file formats are provided by Mapzen: terrarium, 
-#'                    normal, geotiff, and skadi.  Currently only the geotiff
-#'                    format is supported
 #' @export
 #' @examples 
 #' library(quickmapr)
@@ -114,9 +57,7 @@ get_srtm <- function(location,res){
 #' wgs84_dd <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
 #' lake_dd <- spTransform(lake,CRS(wgs84_dd))
 #' get_mapzen_terrain(bbox(lake_dd),z=13)
-get_mapzen_terrain <- function(bbx, z=10, api_key = getOption("mapzen_key"),
-                               file_format = c("geotiff"),
-                               ...){
+get_mapzen_terrain <- function(bbx, z=10, api_key = getOption("mapzen_key")){
   
   web_merc <- "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs"
   wgs84_dd <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
