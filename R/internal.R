ea6630d468578303c3a96d57619f82c62bb34f60
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2016-10-27 12:46:10 -0400

    started more internals

--- R/internal.R
+++ R/internal.R
@@ -1,5 +1,4 @@
 #' function to convert lat long to xyz tile
-#' @keywords internal
 latlong_to_tilexy <- function(lat_deg, lon_deg, zoom){
   #Code from http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#Coordinates_to_tile_numbers_2
   lat_rad <- lat_deg * pi /180
@@ -10,9 +9,10 @@ latlong_to_tilexy <- function(lat_deg, lon_deg, zoom){
 }
 
 #' function to get a data.frame of all xyz tiles to download
-#' @keywords internal
 get_tilexy <- function(bbx,z){
   min_tile <- latlong_to_tilexy(bbx[2,1],bbx[1,1],z)
   max_tile <- latlong_to_tilexy(bbx[2,2],bbx[1,2],z)
   return(expand.grid(min_tile[1]:max_tile[1],min_tile[2]:max_tile[2]))
 }
+
+#' function to check input type and set projection
