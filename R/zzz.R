260b3e986e0cfe7984cf59421cfc6abebcf8b4ce
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2023-09-11 11:12:48 -0400

    typo is startup message.

--- R/zzz.R
+++ R/zzz.R
@@ -2,10 +2,10 @@ op <- options()
 
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage(
-"elevatr v0.99.0 NOTE: Version 0.99.0 of 'elevatr' uses 'sf' and 'terra'.  
-Support for the 'sp', 'raster' (and underlying 'rgdal') packages is being 
-deprecated; however, get_elev_raster continues to return a RasterLayer.  
-This will be dropped in future versions, so please plan accordingly.")
+"elevatr v0.99.0 NOTE: Version 0.99.0 of 'elevatr' uses 'sf' and 'terra'.  Use 
+of the 'sp', 'raster', and underlying 'rgdal' packages by 'elevatr' is being 
+deprecated; however, get_elev_raster continues to return a RasterLayer.  This 
+will be dropped in future versions, so please plan accordingly.")
 }
 
 .onUnload <- function(libname, pkgname){
