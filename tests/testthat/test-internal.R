f160a29f792c90f53c97d884bba8efcdc931ec67
your name <eli.pousson@gmail.com>
2024-08-20 00:59:27 -0400

    Fix test for loc_check

--- tests/testthat/test-internal.R
+++ tests/testthat/test-internal.R
@@ -11,8 +11,8 @@ ll_prj  <- 4326
 aea_prj <- 5072
 
 sf_sm <- st_as_sf(pt_df, coords = c("x", "y"), crs = ll_prj)
-sf_sm_prj <- st_transform(sf_sm, crs = aea_prj) 
-bad_sf <- st_as_sf(data.frame(x = 1000, y = 1000), coords = c("x", "y"), 
+sf_sm_prj <- st_transform(sf_sm, crs = aea_prj)
+bad_sf <- st_as_sf(data.frame(x = 1000, y = 1000), coords = c("x", "y"),
                    crs = ll_prj)
 rast <- terra::rasterize(st_coordinates(sf_sm),terra::rast(sf_sm))
 sf_sm_na <- sf_sm
@@ -26,24 +26,24 @@ mts <- rbind(mt_wash,mt_mans)
 mts$name <- c("Mount Washington", "Mount Mansfield")
 
 test_that("data frame with more extra columns work", {
-  
+
   mts_with_names_and_elevation <- get_elev_point(mts, ll_prj)
   expect_true("name" %in% names(mts_with_names_and_elevation))
 })
 
 test_that("proj_expand works",{
-  
-  mans_sf <- st_as_sf(data.frame(x = -72.8145, y = 44.5438), 
+
+  mans_sf <- st_as_sf(data.frame(x = -72.8145, y = 44.5438),
                       coords = c("x","y"),
                       crs = ll_prj)
   mans <- get_elev_raster(locations =  mans_sf, z = 6)
   mans_exp <- get_elev_raster(locations = mans_sf, z = 6, expand = 2)
-  
+
   rast_elev <- get_elev_raster(locations = rast, z = 5)
   rast_elev_exp <- get_elev_raster(locations = rast, z = 5, exp = 5)
   expect_gt(ncell(mans_exp),ncell(mans))
   expect_gt(ncell(rast_elev_exp), ncell(rast_elev))
-  
+
   origin_sf <- st_as_sf(data.frame(x = 0, y = 0), coords = c("x", "y"),
                         crs = ll_prj)
   origins <- get_elev_raster(locations = origin_sf, z = 6)
@@ -58,14 +58,14 @@ test_that("loc_check errors correctly", {
   expect_error(get_elev_point(locations = rast_na),
                "Please supply a valid crs via locations or prj.")
   expect_error(get_elev_point(locations = sf_sm_na),
-               "Please supply an sf object with a valid crs.")
+               "Please supply an sf or sfc object with a valid crs.")
 })
 
 test_that("Z of 1 or 0 works in get_tilexy",{
 
   sf_sm_1 <- get_elev_raster(sf_sm, z = 1, clip = "bbox")
   sf_sm_0 <- get_elev_raster(sf_sm, z = 0, clip = "bbox")
-  
+
   expect_gt(max(res(sf_sm_1)), 0.27)
   expect_gt(max(res(sf_sm_0)), 0.54)
 })
